f5b4446a3aff014a17f8eba897a31e5856235f27
passerbyYSQ <1127664027@qq.com>
2025-02-23 22:22:24 +0800

    draft

--- update-version.sh
+++ update-version.sh
@@ -46,15 +46,8 @@ old_name=$(git config --local user.name)
 old_email=$(git config --local user.email)
 git config --local user.name "$temp_name"
 git config --local user.email "$temp_email"
-
-# 修复在Git Actions中临时用户名和邮箱不生效
-export GIT_AUTHOR_NAME="$temp_name"
-export GIT_COMMITTER_NAME="$temp_name"
-export GIT_AUTHOR_EMAIL="$temp_email"
-export GIT_COMMITTER_EMAIL="$temp_email"
-
 git status --porcelain | grep 'pom.xml$' | awk '{print $2}' | xargs git add
-git commit -m "Auto-increment version: $root_version"
+git commit --author="$temp_name <$temp_email>"  -m "Auto-increment version: $root_version"
 git push
 git config --local user.name "$old_name"
 git config --local user.email "$old_email"
