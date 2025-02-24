1552e58b9df12cd347904c0f3e8f66d49a5ab442
姚世权 <yaoshiquan@zwcad.com>
2025-02-24 11:54:28 +0800

    模拟文件改动-6

--- src/main/java/top/ysqorz/forum/ForumApplication.java
+++ src/main/java/top/ysqorz/forum/ForumApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
- * 模拟文件改动11222333444555
+ * 模拟文件改动11222333444555666
  */
 @SpringBootApplication
 // 扫描Mapper接口
