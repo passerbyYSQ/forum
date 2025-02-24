fb35cbbfbaa7cc5367e724cb4a3b377b4e343c39
passerbyYSQ <1127664027@qq.com>
2025-02-24 13:48:58 +0800

    模拟文件改动-13

--- src/main/java/top/ysqorz/forum/ForumApplication.java
+++ src/main/java/top/ysqorz/forum/ForumApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
- * 模拟文件改动11222333444555666777888999 AABc
+ * 模拟文件改动11222333444555666777888999 AABcd
  */
 @SpringBootApplication
 // 扫描Mapper接口
