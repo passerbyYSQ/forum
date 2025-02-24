e8363b5b326b764c51b07f64058732ddd688f74b
姚世权 <yaoshiquan@zwcad.com>
2025-02-24 12:50:54 +0800

    draft

--- update-version.sh
+++ update-version.sh
@@ -7,7 +7,7 @@ set -e
 #echo "all_module_paths: $all_module_paths"
 
 # 获取源码文件发生改动的模块的相对路径
-changed_module_paths=$(git diff --name-only HEAD^ HEAD | grep 'src/' | awk -F'/src/' '{print "./"$1""}' | sort -u)
+changed_module_paths=$(git diff --name-only HEAD^ HEAD | grep 'src/' | awk -F'src/' '{print "./"$1""}' | sort -u)
 #echo "changed_module_paths: $changed_module_paths"
 
 # 如果没有识别出改动的模块，则正常退出，防止CI/CD失败
@@ -30,7 +30,7 @@ fi
 
 project_path=$(readlink -f ".")
 echo "$project_path"
-root_version=""
+description=""
 #for module_path in "${update_pom_modules[@]}"; do
 for module_path in $changed_module_paths; do
   # 去到改动模块的目录。注意这是相对于项目根目录的路径
@@ -39,25 +39,24 @@ for module_path in $changed_module_paths; do
   version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
   new_version=$(echo "$version" | awk -F'[.]' '{printf "%d.%d.%d", $1, $2, $3+1}')
   echo "path=$module_path, artifact_id=${artifact_id}, version=$version, new_version=$new_version"
-  if [[ "$module_path" == "." ]]; then
-		root_version=$new_version
-	fi
+  description+="${artifact_id}-$new_version; "
 	# 执行模块升版。约定主分支的版本号形如x.y.z
   mvn versions:set -q -DnewVersion="$new_version" -DartifactId="$artifact_id" -DgenerateBackupPoms=false -DupdateMatchingVersions=false -DprocessDependencies=false
   # 重新回到项目根目录
   cd "$project_path"
 done
 
-echo "Auto-increment version: $root_version"
+description="${description:0:-2}"
+echo "Auto-increment version: $description"
 
 # 备份旧的用户和邮箱信息，以便在CI/CD之后能够还原
 old_name=$(git config --local user.name)
 old_email=$(git config --local user.email)
 git config --local user.name "GitHub Actions Robot"
 git config --local user.email "1127664027@qq.com"
-git status --porcelain | grep 'pom.xml$' | awk '{print $2}' | xargs git add
-git commit -m "Auto-increment version: $root_version"
-git push
+git add "**pom.xml"
+git commit -m "Auto-increment version: $description"
+#git push
 # 还原原本的用户和邮箱信息
 git config --local user.name "$old_name"
 git config --local user.email "$old_email"
